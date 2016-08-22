aa9d4bf2afda0a16b393733eb5ac272b37e72dec
szyugh <julianheinen@gmx.de>
2016-08-22 14:00:09 +0200

    removed index from manager in project

--- db/migrate/20160822114740_create_projects.rb
+++ db/migrate/20160822114740_create_projects.rb
@@ -3,7 +3,7 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string :title
       t.text :description
-      t.belongs_to :manager, :class_name => 'User', index: true, :foreign_key => 'manager_id', :references => 'User'
+      t.belongs_to :manager, :class_name => 'User', :foreign_key => 'manager_id', :references => 'User'
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822113436) do
+ActiveRecord::Schema.define(version: 20160822114740) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "manager_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
