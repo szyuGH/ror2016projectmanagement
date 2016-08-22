2001db469998622455044c1c7b9311136b0a7226
szyugh <julianheinen@gmx.de>
2016-08-22 14:20:03 +0200

    tried to fix migration error with deleting foreign key

--- db/migrate/20160822114740_create_projects.rb
+++ db/migrate/20160822114740_create_projects.rb
@@ -3,7 +3,6 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string :title
       t.text :description
-      t.foreign_key :users, :column => 'manager_id'
 
       t.timestamps null: false
     end
