b367daffdfecd975f4663e34f0f11242230391e2
szyugh <julianheinen@gmx.de>
2016-08-23 23:16:58 +0200

    tried to fix bad request error by specifying region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
+    region: 'eu-central-1',
     aws_access_key_id: ENV['S3_KEY'],
     aws_secret_access_key: ENV['S3_SECRET'],
     endpoint: "https://s3.amazonaws.com",
@@ -17,5 +18,4 @@ CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads" # to let CarrierWave work on Heroku
   config.fog_directory = ENV['S3_BUCKET_NAME']
 
-  Excon.defaults[:nonblock] = false
 end
