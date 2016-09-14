35422204530678e36bcb0925162e0a7f0d24ada9
szyugh <julianheinen@gmx.de>
2016-09-14 21:19:47 +0200

    added additional fields to edit account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,5 +5,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def configure_permitted_params
       devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:login, :first_name, :last_name])
     end
 end
